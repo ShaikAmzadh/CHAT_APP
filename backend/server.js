f48ec501cc397a8292bf6a9ecde8217790b8fc41
Shaik Amzadh <shaikamzadh425@gmail.com>
2025-05-21 16:38:49 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
 import authRoutes from './routes/authRoutes.route.js'
 import messageRoutes from './routes/messageRoutes.route.js'
@@ -12,6 +13,8 @@ import {app,server,io} from './socket/socket.js'
 
 const port=process.env.PORT||5000
 
+const __dirname=path.resolve()
+
 // SO WE CAN USE .ENV VARIABLES IN THIS FILE
 dotenv.config()
 
@@ -21,6 +24,10 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+
 
 
 server.listen(port,()=>{
